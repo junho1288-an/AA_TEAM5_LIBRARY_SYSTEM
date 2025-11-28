33cc92077beb4b04477171824743381c35e2bd1f
Kyungeun Shin <ke819.shin@samsung.com>
2025-11-28 14:58:29 +0900

    Add AuthService

---
+++ LibrarySystem/Controller/AuthService.java
@@ -0,0 +1,55 @@
+package Project.LibrarySystem.Controller;
+
+import Project.LibrarySystem.Model.User;
+import Project.LibrarySystem.Model.Student;
+import Project.LibrarySystem.Model.Librarian;
+import java.util.HashSet;
+import java.util.Set;
+
+public class AuthService {
+    private UserManager userManager;
+    private Set<String> activeSessions; // Stores userIds of logged-in users
+
+    public AuthService(UserManager userManager) {
+        this.userManager = userManager;
+        this.activeSessions = new HashSet<>();
+    }
+
+    public boolean login(String userId, String password) {
+        if (userManager.login(userId, password)) {
+            activeSessions.add(userId);
+            System.out.println("User " + userId + " logged in successfully.");
+            return true;
+        }
+        System.out.println("Login failed for user " + userId);
+        return false;
+    }
+
+    public void logout(String userId) {
+        if (activeSessions.contains(userId)) {
+            activeSessions.remove(userId);
+            System.out.println("User " + userId + " logged out.");
+        }
+    }
+
+    public boolean isAuthenticated(String userId) {
+        return activeSessions.contains(userId);
+    }
+
+    public boolean isStudent(String userId) {
+        User user = userManager.getUser(userId);
+        return user != null && user instanceof Student;
+    }
+
+    public boolean isLibrarian(String userId) {
+        User user = userManager.getUser(userId);
+        return user != null && user instanceof Librarian;
+    }
+    
+    public User getCurrentUser(String userId) {
+        if (isAuthenticated(userId)) {
+            return userManager.getUser(userId);
+        }
+        return null;
+    }
+}
